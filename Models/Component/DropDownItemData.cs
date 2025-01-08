5cf92d960a8c3aa490b5b453c5275d347fc7878b
HoangVan-Tam <hoangvantam.2909@gmail.com>
2025-01-08 19:38:43 +0700

    updated

---
+++ Models/Component/DropDownItemData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Component
+{
+    public class DropDownItemData<T> where T : class
+    {
+        public Guid Id { get; set; } = new Guid();
+        public T Code { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ SMSDOME-Standard-Contest-BlazorServer/Pages/Component/MultipleDropDownList.razor.cs
@@ -0,0 +1,57 @@
+﻿using Entities.Component;
+using Microsoft.AspNetCore.Components;
+
+namespace SMSDOME_Standard_Contest_BlazorServer.Pages.Component
+{
+    public partial class MultipleDropDownList<T> where T : class
+    {
+        [Parameter]
+        public List<DropDownItemData<T>> DataSource { get; set; } = new List<DropDownItemData<T>>();
+        [Parameter]
+        public string Value { get; set; } = "";
+        [Parameter]
+        public string Label { get; set; } = "Data";
+        [Parameter]
+        public EventCallback<object> ValueChanged { get; set; }
+
+
+        private string Text;
+        private bool Active { get; set; } = false;
+        private string Search { get; set; }
+        private List<DropDownItemData<T>> SearchDataSource { get; set; } = new List<DropDownItemData<T>>();
+        private List<DropDownItemData<T>> SelectedItems { get; set; }
+        protected override void OnParametersSet()
+        {
+            SearchDataSource = DataSource;
+            SelectedItems = new List<DropDownItemData<T>>();
+        }
+
+        private void OnClickedItem(DropDownItemData<T> item)
+        {
+
+            if (SelectedItems.Contains(item))
+            {
+                SelectedItems.Remove(item);
+
+            }
+            else
+            {
+                SelectedItems.Add(item);
+            }
+            Text = string.Join(",", SelectedItems.Select(p => p.Text));
+            Value = string.Join(",", SelectedItems.Select(p => p.Code));
+        }
+        private void SearchChanged(ChangeEventArgs __e)
+        {
+            Search = __e?.Value?.ToString();
+            if (!string.IsNullOrEmpty(Search))
+            {
+                SearchDataSource = DataSource.Where(p => p.Text.ToLower().Contains(Search.ToLower())).ToList();
+            }
+            else
+            {
+                SearchDataSource = DataSource;
+            }
+        }
+    }
+}
